fee8edee59406b0d9ae5b8b3ecc7f507217f3fa4
Lahut Sriharun <lahut.s@ku.th>
2018-12-31 11:14:43 +0700

    Add vector.R

---
+++ vector.R
@@ -0,0 +1,15 @@
+# create vector with c()
+
+scores <- c(80,99,90,72,55)
+
+scores_2 <- c(5,8,7,9,10)
+new_score <- scores+scores_2
+
+#Push data from vector
+new_score[1]
+new_score[1:3]
+new_score[3:5]
+new_score[4:5]
+# [start:end]
+new_score[1:20]
+#out of vector will show NA
